36f97b379d754c12119ac8944d5bdd5987be77ff
farzanacoder <farzanaashif10@gmail.com>
2025-09-27 19:26:18 +0600

    Navber done

---
+++ constant/constant.ts
@@ -0,0 +1,38 @@
+export const Navlinks = [
+    {
+        id: 1,
+        url: '#',
+        label: 'Home',
+    },
+    {
+        id: 1,
+        url: '#',
+        label: 'Services',
+    },
+    {
+        id: 1,
+        url: '#',
+        label: 'Resume',
+    },
+    {
+        id: 1,
+        url: '#',
+        label: 'Works',
+    },
+    {
+        id: 1,
+        url: '#',
+        label: 'Skills',
+    },
+    {
+        id: 1,
+        url: '#',
+        label: 'Testimonials',
+    },
+    {
+        id: 1,
+        url: '#',
+        label: 'Conatct',
+    },
+   
+]
