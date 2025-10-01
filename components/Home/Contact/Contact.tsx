062231fea8a95ba3b6dc27af573dc75e15ecf879
farzanacoder <farzanaashif10@gmail.com>
2025-10-01 22:33:03 +0600

    Contact part done

--- components/Home/Contact/Contact.tsx
+++ components/Home/Contact/Contact.tsx
@@ -40,6 +40,14 @@ const Contact = () => {
                 </div>
             </div>
         </div>
+
+            <div className='md:p-10 p-5 bg-[#131332] rounded-lg'>
+                <input type="text" placeholder='Name' className='px-4 py-3.5 bg-[#363659] text-white outline-none rounded-md w-full placeholder:text-white/70'/>
+                <input type="email" placeholder='Email Address' className='px-4 py-3.5 my-6 bg-[#363659] text-white outline-none rounded-md w-full placeholder:text-white/70'/>
+                <input type="text" placeholder='Mobile Number' className='px-4 py-3.5 bg-[#363659] text-white outline-none rounded-md w-full placeholder:text-white/70'/>
+                <textarea name="msg" id="" cols="30" rows="10" placeholder='Your Message' className='px-4 py-3.5 mt-6 bg-[#363659] h-[10rem] text-white outline-none rounded-md w-full placeholder:text-white/70'></textarea>
+                <button className='mt-8 px-12 py-4 bg-blue-950 hover:bg-blue-900 transition-all cursor-pointer text-white rounded-full duration-300'>Send Message</button>
+            </div>
       </div>
     </div>
   )
